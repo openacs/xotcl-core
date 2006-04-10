2df615a839ec5bc83f72778573cbffac5c32e61b
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2006-04-10 10:22:12 +0000

    added a before-install callback to check, whether xotcl is loaded into oacs

---
+++ tcl/install-check-procs.tcl
@@ -0,0 +1,14 @@
+ad_proc ::xotcl::before_install_callback {} {
+  
+  Callback for checking whether xotcl is installed for OpenACS
+  
+  @author Gustaf Neumann (neumann@wu-wien.ac.at)
+} {
+  ns_log notice "-- before-install callback"
+  if {[info command ::xotcl::Class] eq ""} {
+    error " XOTcl does not appear to be installed on your system!\n\
+     Please follow the install instructions on http://www.openacs.org/xowiki/pages/en/xotcl-core"
+  } else {
+    ns_log notice "XOTcl [package require XOTcl] appears to be installed on your system."
+  }
+}
