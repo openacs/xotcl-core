21405e7b16ab5d1d320f62ff3b845bf8c6e091fe
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2008-08-25 10:39:37 +0000

    fix for Oracle style date formats

--- tcl/05-db-procs.tcl
+++ tcl/05-db-procs.tcl
@@ -1710,8 +1710,12 @@ namespace eval ::xo::db {
   } {
     upvar $tz_var tz
     set tz 00
-    if {![regexp {^([^.]+)[.][0-9]*([+-][0-9]*)$} $timestamp _ timestamp tz]} {
-      regexp {^([^.]+)([+-][0-9]*)$} $timestamp _ timestamp tz
+    # Oracle style format like 2008-08-25 (no TZ)
+    if {![regexp {^([0-9]+-[0-9]+-[0-9]+)$} $timestamp _ timestamp]} {
+      # PostgreSQL type ANSI format
+      if {![regexp {^([^.]+)[.][0-9]*([+-][0-9]*)$} $timestamp _ timestamp tz]} {
+        regexp {^([^.]+)([+-][0-9]*)$} $timestamp _ timestamp tz
+      }
     }
     return $timestamp
   }
