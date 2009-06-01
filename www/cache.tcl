5e281f36479e907913806c7c0e69fb7e1f799260
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2009-06-01 07:45:59 +0000

    - improve cache listings

--- www/cache.tcl
+++ www/cache.tcl
@@ -4,6 +4,7 @@ ad_page_contract {
   {cache:optional 0}
   {item:optional 0}
   {flush:optional 0}
+  {filter ""}
   {flushall:optional 0}
 } -properties {
     title:onevalue
@@ -68,11 +69,27 @@ if { $cache == 0 } {
   set item_count [llength $item_list]
   append output "<a href='?cache=$cache&flushall=1'>flush all</a> items of $cache"
 
-  append output "<h3>Items in cache $cache ($item_count) with size [ns_cache_size $cache]</h3><ul>"
+  append output "<h3>Items in cache $cache ($item_count) with size [ns_cache_size $cache]</h3>\n"
+  append output "<form>
+    <input type='hidden' name='cache' value='$cache'>
+    Filter: <input name='filter' value='$filter'>
+    </form>
+  "
+  set entries "<ul>"
+  set count 0
   foreach name [lsort -dictionary $item_list] {
-    append output "<li><a href='?cache=$cache&item=$name'>$name</a> (<a href='?cache=$cache&flush=$name'>flush</a>)</li>"
+    set entry [ns_cache get $cache $name]
+    if {$filter ne ""} {if {![regexp $filter $entry]} continue}
+    incr count
+    set n ""
+    regexp -- {-set name ([^\\]+)\\} $entry _ n
+    append entries "<li><a href='?cache=$cache&item=$name'>$name</a> $n (<a href='?cache=$cache&flush=$name'>flush</a>)</li>"
   }
-  append output "</ul>"
+  append entries "</ul>"
+  if {$filter ne ""} {
+    append output "$count matching entries:\n"
+  }
+  append output $entries
   append output "<a href='?'>All Caches</a>"
 }
 
