323b5958ae738b46cf18115085c099adbedc4c41
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2017-11-19 12:33:15 +0000

    remove commented out code

--- tcl/06-param-procs.tcl
+++ tcl/06-param-procs.tcl
@@ -163,10 +163,6 @@ namespace eval ::xo {
                                           -package_id:required
                                         } {
     return [apm_package_key_from_id $package_id]
-    #return [ns_cache eval xotcl_object_type_cache package_key-$package_id {
-    #  ::xo::dc get_value get_package_key \
-    #      "select package_key from apm_packages where package_id = :package_id"
-    #}]
   }
   parameter proc get_package_id_from_package_key {
                                                   -package_key:required
