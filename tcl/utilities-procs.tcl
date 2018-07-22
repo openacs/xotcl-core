c475e030580406b44c7952bb1db0a51df9519753
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-07-22 08:20:28 +0000

    whitespace changes, added editor hints

--- tcl/utilities-procs.tcl
+++ tcl/utilities-procs.tcl
@@ -1,25 +1,32 @@
 ::xo::library doc {
 
-  XoTcl - Utility procs
+  XoTcl - Utility procs for file I/O. Should not be necessary on the
+  longer run.
 
+  @author Gustaf Neumann
 }
 
 namespace eval ::xo {
 
   proc read_file {fn} {
     set F [open $fn]
-    fconfigure $F -translation binary
-    set content [read $F]
-    close $F
+    ::fconfigure $F -translation binary
+    ::set content [read $F]
+    ::close $F
     return $content
   }
-  
+
   proc write_file {fn content} {
     set F [::open $fn w]
     ::fconfigure $F -translation binary -encoding binary
     ::puts -nonewline $F $content
     ::close $F
-  }    
-
-
+  }
 }
+
+#
+# Local variables:
+#    mode: tcl
+#    tcl-indent-level: 2
+#    indent-tabs-mode: nil
+# End:
