aed1de04b2a009bc9088ab81f561569f26eed286
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-07-22 08:09:03 +0000

    remove internally used, transitional function ::xo::get_raw_request_body (use ns_getcontnet instead)

--- tcl/utilities-procs.tcl
+++ tcl/utilities-procs.tcl
@@ -20,33 +20,6 @@ namespace eval ::xo {
     ::puts -nonewline $F $content
     ::close $F
   }    
-    
-  nsf::proc ::xo::get_raw_request_body {-as_string:switch -as_file:switch} {
-    if {$as_string eq $as_file} {
-      error "either -as_string or -as_file must be specified"
-    }
-    set contentfile [ns_conn contentfile]
-    if {$as_file} {
-      #
-      # If the file was not spooled, obtained it via [ns_conn content]
-      # as write it to a file.
-      #
-      if {$contentfile eq ""} {
-        set contentfile [ad_tmpnam]
-        write_file $contentfile [ns_conn content -binary]
-      }
-      set result $contentfile
-    } else {
-      #
-      # Return the result as a string
-      #
-      if {$contentfile eq ""} {
-        set result [ns_conn content -binary]
-      } else {
-        set result [read_file $contentfile]
-      }
-    }
-    return $result
-  }
+
 
 }
