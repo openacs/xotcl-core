1d09068970969efbec599a650711fc04b2f24aab
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2019-02-21 12:30:59 +0000

    report server tag name as well to get precise version info

--- tcl/01-debug-procs.tcl
+++ tcl/01-debug-procs.tcl
@@ -381,7 +381,7 @@ namespace eval ::xo {
     } else {
       append _ [db_driverkey {}]\n
     }
-    append _ "Server:    [ns_info patchlevel] ([ns_info name])\n"
+    append _ "Server:    [ns_info patchlevel] ([ns_info name] [ns_info tag])\n"
     append _ "Tcl:       $::tcl_patchLevel\n"
     append _ "XOTcl:     $::xotcl::version$::xotcl::patchlevel\n"
     append _ "Tdom:      [package req tdom]\n"
