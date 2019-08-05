618155da49407ed8885867dcdb89356b7c65a2ff
Hector Romojaro <hector.romojaro@gmail.com>
2019-08-05 12:27:12 +0000

    Whitespace cleanup

--- tcl/policy-procs.tcl
+++ tcl/policy-procs.tcl
@@ -229,7 +229,7 @@ namespace eval ::xo {
         } else {
           ns_log notice "enforce_permissions: $user_id doesn't have $privilege on $object"
         }
-        
+
         ad_return_forbidden [_ xotcl-core.permission_denied] \
             [_ xotcl-core.policy-error-insufficient_permissions]
       }
