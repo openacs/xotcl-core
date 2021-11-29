01d959db435d24b9514f2d0efd73ee57e4e34c1d
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2021-11-29 13:20:11 +0000

    remove pipe open (although this is just a fallback file, which is normally not called)

--- www/show-class-graph.tcl
+++ www/show-class-graph.tcl
@@ -15,23 +15,29 @@ ad_page_contract {
 set dot_code [::xo::dotcode -dpi $dpi \
                   -with_children $with_children -documented_methods $documented_only \
                   $classes]
-set dot ""
-catch {set dot [::util::which dot]}
-# final resort for cases, where ::util::which is not available
-if {$dot eq "" && [file executable /usr/bin/dot]} {set dot /usr/bin/dot}
-if {$dot eq ""} {ns_return 404 plain/text "dot not found"; ad_script_abort}
+set dot [::util::which dot]
 
-set tmpnam [ad_tmpnam]
-set tmpfile $tmpnam.$format
+if {$dot eq ""} {
+  ns_return 404 plain/text "dot not found"
+  ad_script_abort
+}
 
-#ns_log notice "png $tmpnam dot $tmpnam.dot"
-set f [open "|$dot  -T$format -o $tmpfile" w]; puts $f $dot_code; close $f
-ns_returnfile 200 [ns_guesstype $tmpfile] $tmpfile
-file delete -- $tmpfile
+set stem [ad_tmpnam]
+set dotfile $stem.dot
+set outfile $stem.$format
 
-#set f [open $tmpnam.dot w]; puts $f $dot_code; close $f
-#file delete -- $tmpnam.dot
-
+try {
+  set f [open $dotfile w]; puts $f $dot_code; close $f
+  exec $dot -T$format -o $outfile $dotfile
+} on error {errorMsg} {
+  ns_log warning "show-class-graph: dot returned $errorMsg"
+  ad_return_error "dot error" $errorMsg
+} on ok {result} {
+  ns_returnfile 200 [ns_guesstype $outfile] $outfile
+  file delete -- $outfile
+} finally {
+  file delete -- $dotfile
+}
 
 #
 # Local variables:
