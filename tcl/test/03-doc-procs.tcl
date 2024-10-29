b6098b5b2f7f0d9ead367f813549ab11c4048e0c
Antonio Pisano <antonio@elettrotecnica.it>
2024-10-29 11:58:21 +0000

    Test updating the nx documentation in a certain cornercase condition
    Reproduces behavior found in the wild

---
+++ tcl/test/03-doc-procs.tcl
@@ -0,0 +1,37 @@
+ad_library {
+  Test of parts of the xotcl-core API
+}
+
+
+aa_register_case -cats {
+  api smoke
+} -procs {
+    "::xo::api update_object_doc"
+    "::xo::api update_nx_docs"
+} xo_update_object_doc {
+
+    Test rebuilding object documentation in relevant corner cases.
+
+} {
+    namespace eval ::__test_xo {
+        nx::Class create AClass {
+            :public method "blueprint get info" {} {
+                #
+                # A method with a cornercase name meant to potentially
+                # collide with other "special words" in nsf.
+                #
+            }
+        }
+    }
+
+    try {
+        ::xo::api update_nx_docs
+    } on ok {d} {
+        set failed_p false
+    } on error {errmsg} {
+        set failed_p true
+        aa_log "Regenerating the docs reports: $errmsg"
+    }
+
+    aa_false "Updating the docs works" $failed_p
+}
